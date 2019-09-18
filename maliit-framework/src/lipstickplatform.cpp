c5fe1a41503dc44910a5ee5a9c20fac6622aebbe
Andrew den Exter <andrew.den.exter@jolla.com>
2019-09-18 16:32:27 +1000

    [maliit] Set the opaque region of the window to same as the input region. Contributes to JB#47331

--- maliit-framework/src/lipstickplatform.cpp
+++ maliit-framework/src/lipstickplatform.cpp
@@ -1,6 +1,7 @@
 /* * This file is part of Maliit framework *
  *
  * Copyright (C) 2013 Jolla Ltd.
+ * Copyright (C) 2019 Open Mobile Platform LLC.
  *
  * Contact: maliit-discuss@lists.maliit.org
  *
@@ -17,8 +18,31 @@
 #include <QPlatformSurfaceEvent>
 #include <qpa/qplatformnativeinterface.h>
 
+#include <wayland-client.h>
+
 namespace Maliit
 {
+
+static void setWaylandInputRegion(QPlatformNativeInterface *wliface, QWindow *window, const QRegion &region)
+{
+    if (wl_compositor *wlcompositor = static_cast<wl_compositor *>(
+                wliface->nativeResourceForIntegration("compositor"))) {
+        if (wl_surface *wlsurface = static_cast<wl_surface *>(
+                    wliface->nativeResourceForWindow("surface", window))) {
+            wl_region *wlregion = wl_compositor_create_region(wlcompositor);
+
+            for (const QRect &rect : region.rects()) {
+                wl_region_add(wlregion, rect.x(), rect.y(), rect.width(), rect.height());
+            }
+
+            wl_surface_set_input_region(wlsurface, wlregion);
+            wl_surface_set_opaque_region(wlsurface, wlregion);
+            wl_region_destroy(wlregion);
+
+            wl_surface_commit(wlsurface);
+        }
+    }
+}
 
 class LipstickWindowPropertyBroadcaster : public QObject
 {
@@ -45,6 +69,8 @@ public:
 
                 native->setWindowProperty(handle, QStringLiteral("CATEGORY"), QStringLiteral("overlay"));
                 native->setWindowProperty(handle, QStringLiteral("MOUSE_REGION"), m_window->property("MOUSE_REGION"));
+
+                setWaylandInputRegion(native, m_window, m_window->property("MOUSE_REGION").value<QRegion>());
             }
             return false;
         }
@@ -56,6 +82,7 @@ public:
                 QPlatformNativeInterface *native = QGuiApplication::platformNativeInterface();
 
                 native->setWindowProperty(handle, QStringLiteral("MOUSE_REGION"), m_window->property("MOUSE_REGION"));
+                setWaylandInputRegion(native, m_window, m_window->property("MOUSE_REGION").value<QRegion>());
             }
             return false;
         }
@@ -75,8 +102,8 @@ void LipstickPlatform::setupInputPanel(QWindow* window, Maliit::Position)
 
 void LipstickPlatform::setInputRegion(QWindow* window, const QRegion& region)
 {
-    window->setProperty("MOUSE_REGION", region);
     window->setMask(region);
+    window->setProperty("MOUSE_REGION", region);
 }
 
 } // namespace Maliit
