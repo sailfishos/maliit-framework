9ce24f843276892e3a8a7b3e9aa7006071baabb8
Simo Piiroinen <simo.piiroinen@jolla.com>
2021-03-31 14:49:33 +0300

    [maliit] Use non-abstract unix domain socket. JB#52254
    Abstract unix socket paths are problematic from firejail
    sandboxing point of view as bind and connect must happen
    from the same net namespace. As maliit use must in practice
    be allowed to all applications, this makes it impossible to
    detach applications from the global net namespace.
    
    Switching to regular file based unix sockets solves the net
    namespace problem and allows use of firejail black/whitelist
    mechanisms for controlling access to maliit socket.
    
    Using an extra directory level for socket makes it possible
    to grant bind permission without simultaneously allowing
    removal / replacement of other unrelated unix sockets.
    
    Signed-off-by: Simo Piiroinen <simo.piiroinen@jolla.com>

--- maliit-framework/connection/serverdbusaddress.cpp
+++ maliit-framework/connection/serverdbusaddress.cpp
@@ -1,7 +1,8 @@
 /* * This file is part of Maliit framework *
  *
- * Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+ * Copyright (c) 2010 Nokia Corporation and/or its subsidiary(-ies).
  * All rights reserved.
+ * Copyright (c) 2021 Jolla Ltd.
  *
  * Contact: maliit-discuss@lists.maliit.org
  *
@@ -18,8 +19,13 @@
 #include <QDBusConnection>
 
 #include <QDBusServer>
+#include <QDir>
 
 #include <cstdlib>
+
+#include <sys/stat.h>
+
+#include <unistd.h>
 
 namespace {
     const char * const MaliitServerName = "org.maliit.server";
@@ -62,12 +68,33 @@ DynamicAddress::DynamicAddress()
 
 QDBusServer* DynamicAddress::connect()
 {
-    QLatin1String dbusAddress("unix:tmpdir=/tmp/maliit-server");
-
-    QDBusServer *server = new QDBusServer(dbusAddress);
-
-    publisher.reset(new AddressPublisher(server->address()));
-
+    QDBusServer *server = nullptr;
+    char *runDir = nullptr;
+    char *socketDir = nullptr;
+    char *socketFile = nullptr;
+    const char *env = getenv("XDG_RUNTIME_DIR");
+    if (env && *env == '/' && QDir(QString::fromUtf8(env)).exists())
+        runDir = strdup(env);
+    else if (asprintf(&runDir, "/run/user/%d", static_cast<int>(getuid())) < 0)
+        runDir = nullptr;
+    if (!runDir) {
+        qWarning("Could not determine XDG_RUNTIME_DIR");
+    } else if (asprintf(&socketDir, "%s/maliit-server", runDir) < 0) {
+        socketDir = nullptr;
+    } else if (asprintf(&socketFile, "%s/dbus-socket", socketDir) < 0) {
+        socketFile = nullptr;
+    } else if (mkdir(socketDir, 0770) == -1 && errno != EEXIST) {
+        qWarning("Failed to create %s: %s", socketDir, strerror(errno));
+    } else if (unlink(socketFile) == -1 && errno != ENOENT) {
+        qWarning("Failed to remove %s: %s", socketFile, strerror(errno));
+    } else {
+        QString dbusAddress(QStringLiteral("unix:path=%1").arg(socketFile));
+        server = new QDBusServer(dbusAddress);
+        publisher.reset(new AddressPublisher(server->address()));
+    }
+    free(socketFile);
+    free(socketDir);
+    free(runDir);
     return server;
 }
 
